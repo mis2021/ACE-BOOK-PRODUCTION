aa7882c418f7d0b6cba941373629087f03a50595
Jacky Avenido <jackyavenido.se@gmail.com>
2022-07-05 22:50:41 +0800

    feed debugging

--- front/src/app/common/feed/feedPosts.tsx
+++ front/src/app/common/feed/feedPosts.tsx
@@ -25,21 +25,19 @@ type StateType = {
   skip: number;
   hasMore: boolean;
   posts: any;
-
+  postLength: number;
 }
 
 const postPerView = 3;
 
 const initialState = {
   posts: [],
-  skip: 3,
+  skip: 0,
   hasMore: true,
-
+  postLength: 0
 }
 
 const FeedPosts = ({ loading, posts, countAll, refetch, currentPage, departmenId, type, refetchQuery }: Props) => {
-
-  
 
   const [state, setState] = useState<StateType>(initialState)
   const {
@@ -48,25 +46,48 @@ const FeedPosts = ({ loading, posts, countAll, refetch, currentPage, departmenId
 
 
   useEffect(() => {
-    if(currentPage == 0){
-      setState((p) => ({ ...p, posts: posts }))
-    }else{
-      setState((p) => ({ ...p, posts: _.concat(state.posts, posts) }))
-     
+    let plength = 3;
+    if (currentPage == 0) {
+      setState((p) => ({ ...p, posts: posts, postLength: plength, hasMore: true, skip: 0 }))
+    } else {
+      let newPost = _.concat(state.posts, posts)
+      plength = newPost.length
+      setState((p) => ({ ...p, posts: newPost, postLength: plength }))
+
     }
   }, [posts])
 
   const fetchMore = () => {
     setTimeout(() => {
-      if(refetchQuery){
-        refetch(refetchQuery)
-      }else{
-        refetch()
-      }
-      
-      setState((p) => ({ ...p, skip: state.skip + postPerView, hasMore: state.posts.length >= countAll ? false : true }))
+      // if (refetchQuery) {
+      //   refetch(refetchQuery)
+      // } else {
+      //   refetch()
+      // }
+      let nSkip = state.skip + postPerView;
+
+      let fq: any = refetchQuery
+      fq.skip = nSkip
+      refetch(fq)
+
+      // setState((p) => ({ ...p, skip: state.skip + postPerView}))
+      setState((p) => ({
+        ...p,
+        skip: state.posts.length >= countAll ? 0 : nSkip,
+        hasMore: currentPage == 0 ? true : (state.posts.length >= countAll ? false : true)
+      }))
     }, 1500);
   }
+
+  // console.log("fetching", {
+  //   posts: posts,
+  //   countAll: countAll,
+  //   currentPage: currentPage,
+  //   refetchQuery: refetchQuery,
+  //   hasMore: state.hasMore,
+  //   skip: state.skip
+  // })
+  // console.log("lenght",state.postLength)
 
   return (
     <div>
@@ -97,15 +118,17 @@ const FeedPosts = ({ loading, posts, countAll, refetch, currentPage, departmenId
                   </div>
                 }
               >
+
                 {
                   state.posts.length >= 1 && <>
                     {
-                      state.posts.map((item: PostFormValues) => (
+                      state.posts.map((item: PostFormValues, index: number) => (
                         // <LazyLoad height={200} debounce={true} throttle={true}>
-                          <PostIndex
-                            data={item}
-                            tags={item.tags}
-                          />
+                        <PostIndex
+                          index={index}
+                          data={item}
+                          tags={item.tags}
+                        />
                         // </LazyLoad>
                       ))
                     }
